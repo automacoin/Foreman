0440c73704374c36b8fd510006d9fd66c306fafe
Cameron Sajedi <cameronsajedi@gmail.com>
2021-04-25 16:00:30 -0700

    in progress queue implementation

--- automa/core/queue.py
+++ automa/core/queue.py
@@ -1,10 +1,10 @@
 class Queue:
-    """Holds the incoming submissions until they can be processed"""
+    """Holds the incoming submissions until they can be processed. For now it is a simple datastore"""
 
     def __init__():
         pass
 
-    def accept():
+    def accept(tape_record):
         pass
 
     def get_debts():
